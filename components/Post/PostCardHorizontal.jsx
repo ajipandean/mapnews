8bb3bc50bfebfbe98e7d8539e9239438eb8f21a0
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-09 18:14:55 +0800

    added floating post card item

---
+++ components/Post/PostCardHorizontal.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Image } from 'react-native-elements';
+
+import useTheme from '../../hooks/useTheme';
+
+export default function PostCardHorizontal({ width }) {
+  const { colors } = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      width,
+      backgroundColor: colors.surface,
+      borderRadius: 4,
+      overflow: 'hidden',
+      marginHorizontal: 8,
+      flexDirection: 'row',
+      elevation: 2,
+    },
+    photo: {
+      width: 120,
+      height: 120,
+      resizeMode: 'cover',
+    },
+    post: {
+      flex: 1,
+      padding: 12,
+    },
+    title: {
+      fontSize: 16,
+      marginBottom: 8,
+      fontWeight: 'bold',
+    },
+    author: {
+      fontSize: 12,
+      color: colors.placeholder,
+    },
+    timestamps: {
+      fontSize: 12,
+      color: colors.placeholder,
+    },
+  });
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.photo}
+        source={{
+          uri:
+            'https://c.files.bbci.co.uk/1AD9/production/_100837860_teslacrash.jpg',
+        }}
+      />
+      <View style={styles.post}>
+        <Text style={styles.title}>Tabrakan Parah! Mobil vs Truk</Text>
+        <Text style={styles.author}>Pandean Mertayasa</Text>
+        <Text style={styles.timestamps}>Selasa, 23 April 2020 at 13:00</Text>
+      </View>
+    </View>
+  );
+}
--- screens/MainBottomTabs/ExploreScreen.jsx
+++ screens/MainBottomTabs/ExploreScreen.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { View, StyleSheet } from 'react-native';
+import {
+  View, FlatList, StyleSheet, Dimensions,
+} from 'react-native';
 
 import useTheme from '../../hooks/useTheme';
 import HeaderBar from '../../components/HeaderBar';
+import PostCardHorizontal from '../../components/Post/PostCardHorizontal';
+
+const { width } = Dimensions.get('window');
+const POST_CARD_WIDTH = width * 0.8;
+const POST_CARD_INSET = width * 0.1 - 5;
 
 export default function ExploreScreen() {
   const { colors } = useTheme();
@@ -12,14 +19,34 @@ export default function ExploreScreen() {
       flex: 1,
       backgroundColor: colors.surface,
     },
-    map: {
-      flex: 1,
+    map: { flex: 1 },
+    slider: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0,
     },
   });
   return (
     <View style={styles.container}>
       <HeaderBar title="Explore" />
       <MapView style={styles.map} />
+      <FlatList
+        horizontal
+        style={styles.slider}
+        data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]}
+        keyExtractor={(item, index) => `${item}-${index}`}
+        contentContainerStyle={{
+          paddingHorizontal: POST_CARD_INSET - 4,
+          paddingVertical: 16,
+        }}
+        scrollEventThrottle={1}
+        showsHorizontalScrollIndicator={false}
+        renderItem={() => <PostCardHorizontal width={POST_CARD_WIDTH} />}
+        pagingEnabled
+        snapToInterval={POST_CARD_WIDTH + 16}
+        snapToAlignment="start"
+      />
     </View>
   );
 }
