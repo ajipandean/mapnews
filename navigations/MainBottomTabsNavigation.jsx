8255b0937ecabb38d4d433845f0609f98ed5a7d0
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-09 16:40:21 +0800

    customized tab bar

--- navigations/MainBottomTabsNavigation.jsx
+++ navigations/MainBottomTabsNavigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from 'react-native-elements';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import mainBottomTabsRegister from '../registers/mainBottomTabsRegister';
@@ -7,13 +8,29 @@ const MainBottomTabs = createBottomTabNavigator();
 
 export default function MainBottomTabsNavigation() {
   return (
-    <MainBottomTabs.Navigator>
+    <MainBottomTabs.Navigator
+      tabBarOptions={{
+        style: { height: 56 },
+        labelStyle: { fontSize: 12 },
+        tabStyle: { paddingVertical: 8 },
+      }}
+    >
       {mainBottomTabsRegister.map((s) => (
         <MainBottomTabs.Screen
           key={s.name}
           name={s.name}
           component={s.component}
-          options={s.options}
+          options={{
+            ...s.options,
+            tabBarIcon: ({ focused, color, size }) => (
+              <Icon
+                name={focused ? s.icon.active : s.icon.inactive}
+                size={size}
+                color={color}
+                type="material-community"
+              />
+            ),
+          }}
         />
       ))}
     </MainBottomTabs.Navigator>
--- registers/mainBottomTabsRegister.jsx
+++ registers/mainBottomTabsRegister.jsx
@@ -3,7 +3,13 @@ import ExploreScreen from '../screens/MainBottomTabs/ExploreScreen';
 export default [
   {
     name: 'explore',
+    icon: {
+      active: 'map-marker',
+      inactive: 'map-marker-outline',
+    },
     component: ExploreScreen,
-    options: {},
+    options: {
+      title: 'Explore',
+    },
   },
 ];
