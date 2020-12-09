a786983a016ad898336a5ef32dc62f1a7d384d0b
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-09 16:45:08 +0800

    added other screens

--- registers/mainBottomTabsRegister.jsx
+++ registers/mainBottomTabsRegister.jsx
@@ -1,4 +1,6 @@
 import ExploreScreen from '../screens/MainBottomTabs/ExploreScreen';
+import CreatePostScreen from '../screens/MainBottomTabs/CreatePostScreen';
+import ProfileScreen from '../screens/MainBottomTabs/ProfileScreen';
 
 export default [
   {
@@ -10,6 +12,28 @@ export default [
     component: ExploreScreen,
     options: {
       title: 'Explore',
+    },
+  },
+  {
+    name: 'create-post',
+    icon: {
+      active: 'plus-circle',
+      inactive: 'plus-circle-outline',
+    },
+    component: CreatePostScreen,
+    options: {
+      title: 'Create',
+    },
+  },
+  {
+    name: 'profile',
+    icon: {
+      active: 'account-circle',
+      inactive: 'account-circle-outline',
+    },
+    component: ProfileScreen,
+    options: {
+      title: 'Profile',
     },
   },
 ];
