9efc54e430cd9a6081923f4ae9dd930f5b575dc0
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-11 14:24:45 +0800

    registered camera screen in root stack

--- registers/rootStackRegister.jsx
+++ registers/rootStackRegister.jsx
@@ -1,9 +1,15 @@
 import MainBottomTabsNavigation from '../navigations/MainBottomTabsNavigation';
+import CameraScreen from '../screens/RootStack/CameraScreen';
 
 export default [
   {
     name: 'main-bottom-tabs',
     component: MainBottomTabsNavigation,
+    options: {},
+  },
+  {
+    name: 'camera',
+    component: CameraScreen,
     options: {},
   },
 ];
