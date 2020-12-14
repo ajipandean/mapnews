1d7f345baa12656302ea060dca23f1ed3d90a7d2
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-15 00:32:44 +0800

    centered form input

--- screens/AuthStack/LoginScreen/styles.jsx
+++ screens/AuthStack/LoginScreen/styles.jsx
@@ -4,6 +4,7 @@ export default ({ statusBarHeight }) => StyleSheet.create({
   top_spaced: { marginTop: 16 },
   container: {
     flex: 1,
+    justifyContent: 'center',
     paddingHorizontal: 16,
     paddingTop: statusBarHeight,
     backgroundColor: '#fff',
