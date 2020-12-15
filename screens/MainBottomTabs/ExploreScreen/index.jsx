09769418d67a1eb0137fd8fbffc97cc93420a246
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-15 20:39:30 +0800

    added header and floating action button

--- screens/MainBottomTabs/ExploreScreen/index.jsx
+++ screens/MainBottomTabs/ExploreScreen/index.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import MapView from 'react-native-maps';
 import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Icon, Header, Text } from 'react-native-elements';
 
 import exploreScreenStyles from './styles';
+import useTheme from '../../../hooks/useTheme';
 
 export default () => {
+  const { colors } = useTheme();
+
   const styles = exploreScreenStyles();
 
   return (
     <View style={styles.container}>
       <MapView style={styles.map} />
+      <Header
+        backgroundColor="transparent"
+        containerStyle={styles.header}
+        centerComponent={() => <Text h4>Mapnews</Text>}
+      />
+      <Icon
+        raised
+        reverse
+        name="plus"
+        type="material-community"
+        color={colors.primary}
+        reverseColor={colors.surface}
+        containerStyle={styles.icon_button}
+        onPress={() => console.log('Add button hitted')}
+      />
+      <StatusBar style="dark" />
     </View>
   );
 };
--- screens/MainBottomTabs/ExploreScreen/styles.jsx
+++ screens/MainBottomTabs/ExploreScreen/styles.jsx
@@ -1,9 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 export default () => StyleSheet.create({
-  container: { flex: 1 },
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
   map: {
     width: '100%',
     height: '100%',
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    borderBottomWidth: 0,
+  },
+  icon_button: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    margin: 16,
   },
 });
