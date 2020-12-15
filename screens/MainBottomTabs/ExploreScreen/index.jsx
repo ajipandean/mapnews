5e078bbc911637f3c48e51dc234f6b5300834cf2
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-15 20:14:51 +0800

    added styles file for explore screen

--- screens/MainBottomTabs/ExploreScreen/index.jsx
+++ screens/MainBottomTabs/ExploreScreen/index.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-elements';
 
-export default () => (
-  <View>
-    <Text>Explore screen</Text>
-  </View>
-);
+import exploreScreenStyles from './styles';
+
+export default () => {
+  const styles = exploreScreenStyles();
+
+  return (
+    <View style={styles.container}>
+      <Text>Explore screen</Text>
+    </View>
+  );
+};
