4e4d5ddf3f109b89fc23dec6e1ac7e2f703d0c93
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-15 12:47:15 +0800

    added validation in auth form input

--- components/AuthFormInput/index.jsx
+++ components/AuthFormInput/index.jsx
@@ -20,9 +20,8 @@ export default ({ mode }) => {
     setLoading(true);
     try {
       if (email.length <= 0) throw new Error('Email cannot be empty');
-      if (confirmPassword !== password) {
-        throw new Error("Confirm password doesn't match");
-      }
+      if (password.length <= 0) throw new Error('Password cannot be empty');
+      if (confirmPassword !== password) throw new Error("Confirm password doesn't match");
       await register(email, password);
     } catch (err) {
       toast(err.message);
@@ -38,6 +37,7 @@ export default ({ mode }) => {
         textContentType="emailAddress"
         label="Alamat email"
         placeholder="ashiap@example.com"
+        returnKeyType="next"
         containerStyle={styles.no_horizontal_padding}
         value={email}
         onChangeText={(v) => setEmail(v)}
@@ -51,6 +51,8 @@ export default ({ mode }) => {
         secureTextEntry={!shown}
         label="Kata sandi"
         placeholder="********"
+        textContentType="password"
+        returnKeyType={mode === 'register' ? 'next' : 'send'}
         containerStyle={[styles.no_horizontal_padding]}
         value={password}
         onChangeText={(v) => setPassword(v)}
@@ -71,6 +73,8 @@ export default ({ mode }) => {
           secureTextEntry={!shown}
           label="Konfirmasi kata sandi"
           placeholder="********"
+          textContentType="newPassword"
+          returnKeyType="send"
           containerStyle={[styles.no_horizontal_padding]}
           value={confirmPassword}
           onChangeText={(v) => setConfirmPassword(v)}
