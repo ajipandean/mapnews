7ba0fa397bd7e4b45ab32af8e235353d6666bab5
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-16 16:25:23 +0800

    added update profile logic

--- screens/RootStack/UpdateProfileScreen/index.jsx
+++ screens/RootStack/UpdateProfileScreen/index.jsx
@@ -1,17 +1,45 @@
 import React, { useState } from 'react';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { ScrollView, View } from 'react-native';
 import { Button } from 'react-native-elements';
 
 import updateProfileScreenStyles from './styles';
+import firebase from '../../../firebase.config';
+import toast from '../../../helpers/toast';
 import UpdateProfileFormInput from '../../../components/UpdateProfileFormInput';
 
 export default () => {
   const { params } = useRoute();
+  const { navigate } = useNavigation();
+  const [loading, setLoading] = useState(false);
   const [photo, setPhoto] = useState(params.user.photoURL);
   const [username, setUsername] = useState(params.user.displayName);
 
   const styles = updateProfileScreenStyles();
+
+  const handleUpdateProfile = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(photo);
+      const blob = await response.blob();
+      const formattedUsername = username.split(' ').join('');
+      const ref = firebase
+        .storage()
+        .ref('photo_profile')
+        .child(`${Date.now()}_${formattedUsername}.jpg`);
+      const snapshot = await ref.put(blob, { contentType: 'image/jpeg' });
+      const downloadURL = await snapshot.ref.getDownloadURL();
+      await params.user.updateProfile({
+        photoURL: downloadURL,
+        displayName: username,
+      });
+      navigate('main-bottom-tabs', { screen: 'profile' });
+    } catch (err) {
+      toast(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -22,7 +50,12 @@ export default () => {
           setPhoto={setPhoto}
           setUsername={setUsername}
         />
-        <Button title="Update" buttonStyle={styles.button} />
+        <Button
+          loading={loading}
+          title="Update"
+          buttonStyle={styles.button}
+          onPress={handleUpdateProfile}
+        />
       </ScrollView>
     </View>
   );
