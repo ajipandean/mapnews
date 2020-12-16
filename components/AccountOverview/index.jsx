c5ead035a53228a4b6b6e358f8287cb5b3681f6e
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-16 14:10:32 +0800

    added logout logic

--- components/AccountOverview/index.jsx
+++ components/AccountOverview/index.jsx
@@ -4,22 +4,28 @@ import { Text, Avatar, Button } from 'react-native-elements';
 
 import accountOverviewStyles from './styles';
 
-export default () => {
+export default ({ user }) => {
   const styles = accountOverviewStyles();
   const avatarPlaceholder = 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png';
 
+  if (!user) return <View />;
   return (
     <View style={[styles.center_horizontal, styles.padded_vertical]}>
-      <Avatar rounded size={100} source={{ uri: avatarPlaceholder }} />
+      <Avatar
+        rounded
+        size={100}
+        source={{ uri: user.photoURL || avatarPlaceholder }}
+      />
       <View style={[styles.margined_vertical, styles.center_horizontal]}>
-        <Text style={styles.username}>Anonymus</Text>
-        <Text style={styles.email}>merth@merthlabs.com</Text>
+        <Text style={styles.username}>{user.displayName || 'anonymus'}</Text>
+        <Text style={styles.email}>{user.email}</Text>
       </View>
       <Button
         type="clear"
         title="Ubah profil"
         buttonStyle={styles.button}
         containerStyle={[styles.compact]}
+        onPress={() => {}}
       />
     </View>
   );
--- screens/MainBottomTabs/ProfileScreen/index.jsx
+++ screens/MainBottomTabs/ProfileScreen/index.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { ScrollView, View } from 'react-native';
 import { Text, Header } from 'react-native-elements';
 
 import profileScreenStyles from './styles';
+import firebase from '../../../firebase.config';
 import useTheme from '../../../hooks/useTheme';
+import toast from '../../../helpers/toast';
+import AuthContext from '../../../contexts/authContext';
 import AccountOverview from '../../../components/AccountOverview';
 
 export default () => {
   const { colors } = useTheme();
+  const { navigate } = useNavigation();
+  const { logout } = useContext(AuthContext);
+  const [user, setUser] = useState({});
 
   const styles = profileScreenStyles();
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((u) => {
+      setUser(u);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch (err) {
+      toast(err.message);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -17,9 +39,14 @@ export default () => {
         containerStyle={styles.header}
         backgroundColor={colors.surface}
         centerComponent={() => <Text h4>Profil</Text>}
+        rightComponent={{
+          icon: 'exit-to-app',
+          type: 'material-community',
+          onPress: handleLogout,
+        }}
       />
       <ScrollView style={styles.padded_horizontal}>
-        <AccountOverview />
+        <AccountOverview user={user} navigate={navigate} />
       </ScrollView>
     </View>
   );
