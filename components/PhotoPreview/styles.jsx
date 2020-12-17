8c11fb7a559795609cafb3dcfc1df07226cb8f18
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-17 18:44:53 +0800

    added save post to firestore logic

--- components/PhotoPreview/styles.jsx
+++ components/PhotoPreview/styles.jsx
@@ -3,10 +3,22 @@ import { StyleSheet } from 'react-native';
 export default (widthDimension) => StyleSheet.create({
   container: {
     height: widthDimension,
+    position: 'relative',
   },
   image: {
     width: '100%',
     height: '100%',
     resizeMode: 'contain',
+  },
+  indicator: { marginBottom: 12 },
+  loading: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- screens/RootStack/CreatePostScreen/index.jsx
+++ screens/RootStack/CreatePostScreen/index.jsx
@@ -5,6 +5,7 @@ import { Button } from 'react-native-elements';
 import Moment from 'moment';
 
 import createPostScreenStyles from './styles';
+import toast from '../../../helpers/toast';
 import firebase from '../../../firebase.config';
 import CreatePostFormInput from '../../../components/CreatePostFormInput';
 import PhotoPreview from '../../../components/PhotoPreview';
@@ -17,15 +18,68 @@ export default () => {
   const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
   const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const styles = createPostScreenStyles();
 
+  const handleSavePost = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(params.result.uri);
+      const blob = await response.blob();
+      const ref = firebase
+        .storage()
+        .ref('posts')
+        .child(`POST_${Date.now()}.jpg`);
+      const uploadTask = ref.put(blob, { contentType: 'image/jpeg' });
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+        },
+        (err) => {
+          throw new Error(err);
+        },
+        async () => {
+          const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
+          try {
+            const db = firebase.firestore();
+            await db.collection('posts').add({
+              title,
+              description: desc,
+              uploaded_at: date,
+              location: {
+                lat: 0,
+                lon: 0,
+              },
+              user,
+              photo: downloadURL,
+            });
+          } catch (err) {
+            toast(err.message);
+          }
+          navigation.navigate('main-bottom-tabs', { screen: 'explore' });
+        },
+      );
+    } catch (err) {
+      toast(err.message);
+    }
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerRight: () => <Button type="clear" title="Simpan" />,
+      headerRight: () => (
+        <Button
+          loading={loading}
+          type="clear"
+          title="Simpan"
+          onPress={handleSavePost}
+        />
+      ),
       headerRightContainerStyle: { marginRight: 8 },
     });
-  }, [navigation]);
+  }, [navigation, title, desc, user, loading]);
 
   useEffect(() => {
     const unsubscribe = firebase
@@ -46,7 +100,7 @@ export default () => {
           setDesc={setDesc}
         />
         <PostMetas date={date} user={user} />
-        <PhotoPreview result={params.result} />
+        <PhotoPreview result={params.result} progress={progress} />
       </ScrollView>
     </View>
   );
