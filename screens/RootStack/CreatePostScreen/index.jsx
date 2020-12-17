d599aa3c01da3366121f60f6ffed25a310216bae
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-17 15:31:47 +0800

    added simpan button

--- screens/RootStack/CreatePostScreen/index.jsx
+++ screens/RootStack/CreatePostScreen/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Text, Button } from 'react-native-elements';
 
-export default () => (
-  <View>
-    <Text>Create post screen</Text>
-  </View>
-);
+export default () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Button type="clear" title="Simpan" />,
+      headerRightContainerStyle: { marginRight: 4 },
+    });
+  }, [navigation]);
+
+  return (
+    <View>
+      <Text>Create post screen</Text>
+    </View>
+  );
+};
