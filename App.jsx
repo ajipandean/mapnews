97dbfcef18dbeebeea1331e4cce4133f011cbce3
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-17 22:15:30 +0800

    ignored specific yellow warning

--- App.jsx
+++ App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import RootStackNavigation from './navigations/RootStackNavigation';
+
+LogBox.ignoreLogs([
+  'Setting a timer for a long period of time',
+  'Non-serializable values were found in the navigation state',
+]);
 
 export default function App() {
   return (
